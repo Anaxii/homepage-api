ee8d193669c4896ce0bed6556cd235e361a9a5a3
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-07-18 00:25:14 -0700

    baskets and tokens endpoint

--- main.go
+++ main.go
@@ -12,38 +12,75 @@ import (
 	"net/http"
 )
 
-var HomeStats interface{}
+type Token struct {
+	Name         string `json:"name"`
+	Token        string `json:"token"`
+	Quote        string `json:"quote"`
+	PairAddress  string `json:"pairAddress"`
+	TokenAddress string `json:"tokenAddress"`
+	QuoteAddress string `json:"quoteAddress"`
+}
 
-func serveHomeStats(w http.ResponseWriter, r *http.Request) {
+type Basket struct {
+	TokenAddress string `json:"tokenAddress"`
+	JoeAddress   string `json:"joeAddress"`
+	Name         string `json:"name"`
+	Symbol       string `json:"symbol"`
+	Tokens       []Token `json:"tokens"`
+}
+
+var HomeStats interface{}
+var Baskets []Basket
+var Tokens []Token
+
+func serve(w http.ResponseWriter, data []byte) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
-	data, _ := json.Marshal(HomeStats)
 	w.Write(data)
+}
+
+func serveHomeStats(w http.ResponseWriter, r *http.Request) {
+	data, _ := json.Marshal(HomeStats)
+	serve(w, data)
+}
+
+func serveBaskets(w http.ResponseWriter, r *http.Request) {
+	data, _ := json.Marshal(Baskets)
+	serve(w, data)
+}
+
+func serveTokens(w http.ResponseWriter, r *http.Request) {
+	data, _ := json.Marshal(Tokens)
+	serve(w, data)
 }
 
 func main() {
 	go fetchTimer()
 	router := mux.NewRouter()
 	router.HandleFunc("/homestats", serveHomeStats)
+	router.HandleFunc("/baskets", serveBaskets)
+	router.HandleFunc("/tokens", serveTokens)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func fetchTimer() {
 	getHomeStats()
+	getBaskets()
+	getTokens()
 	s := gocron.NewScheduler()
 	s.Every(60).Seconds().Do(getHomeStats)
+	s.Every(60).Seconds().Do(getBaskets)
+	s.Every(60).Seconds().Do(getTokens)
 	<- s.Start()
 }
 
-func getHomeStats() {
-
+func getHomeStats() error {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://anaxii:B4ngFestina123@cluster0.1lvz50h.mongodb.net/test"))
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer func() {
 		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
 		}
 	}()
 	coll := client.Database("Exposure").Collection("exposurelandingstats")
@@ -52,8 +89,51 @@ func getHomeStats() {
 	_options.SetSort(bson.M{"$natural":-1})
 	err = coll.FindOne(context.TODO(), bson.M{}, _options).Decode(&result)
 	if err == mongo.ErrNoDocuments {
-		return
+		return err
 	}
 	delete(result, "_id")
 	HomeStats = result
+	return nil
+}
+
+func getBaskets() error {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://anaxii:B4ngFestina123@cluster0.1lvz50h.mongodb.net/test"))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	coll := client.Database("Exposure").Collection("baskets")
+	var result []Basket
+	cursor, err := coll.Find(context.TODO(), bson.D{})
+	ctx := context.Background()
+	if err = cursor.All(ctx, &result); err != nil {
+		return err
+	}
+	Baskets = result
+	return nil
+}
+
+func getTokens() error {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://anaxii:B4ngFestina123@cluster0.1lvz50h.mongodb.net/test"))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	coll := client.Database("Exposure").Collection("tokens")
+	var result []Token
+	cursor, err := coll.Find(context.TODO(), bson.D{})
+	ctx := context.Background()
+	if err = cursor.All(ctx, &result); err != nil {
+		return err
+	}
+	Tokens = result
+	return nil
 }
